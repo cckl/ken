942dea5b3513d5609d9c041bba9bfa19e88f2698
Christina <clingc7@gmail.com>
2018-09-10 15:36:06 +0800

    add form submit success

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ $(document).ready(() => {
       data: form.serialize(),
       success: (res) => {
         console.log('res', res)
+        alert("Thank you. Your message has been sent and Ken will get back to you shortly!")
+        $('#form')[0].reset();
       },
       error: (error) => {
         console.log('error', error)
