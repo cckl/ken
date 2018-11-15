57f593b31c93474f566741eca6264928993dc12a
Jeff Tang <jt26@rice.edu>
2018-11-14 21:06:26 -0600

    disable button on submit

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ $(document).ajaxComplete(function () {
 
 $(document).ready(() => {
   const form = $('#form')
+  const send = $('#send')
   form.on('submit', (e) => {
     e.preventDefault()
+    send.prop('disabled', true)
     $.ajax({
       type: 'POST',
       url: URL + '/users',
@@ -19,11 +21,13 @@ $(document).ready(() => {
       success: (res) => {
         console.log('res', res)
         alert("Thank you. Your message has been sent and Ken will get back to you shortly!")
+        send.prop('disabled', false)
         $('#form')[0].reset()
       },
       error: (error) => {
         console.log('error', error)
         alert("There was an error sending your message. Please ensure you have internet connection and/or refresh the page and try again.")
+        send.prop('disabled', false)
       },
     })
   })
